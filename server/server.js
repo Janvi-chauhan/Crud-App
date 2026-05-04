f361374018c1ba6859d82128619accf8e8b7c7d5
Janvi-chauhan <janvichauhan1223@gmail.com>
2026-05-04 15:17:54 +0530

    Fix root routes

--- server/server.js
+++ server/server.js
@@ -8,23 +8,28 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-console.log("MONGO_URI:", process.env.MONGO_URI);
-
-
-connectDB();
-
-// routes
-app.use("/students", studentRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    console.log("Database Connected");
+
+    app.use("/students", studentRoutes);
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
