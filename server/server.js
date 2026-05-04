871c8d85b2b0b1eedc9b61067435dcd9b6a25e69
Janvi-chauhan <janvichauhan1223@gmail.com>
2026-05-04 15:00:10 +0530

    Latest

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import studentRoutes from "./routes/student.routes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(
   cors({
-    origin: "*", 
+    origin: "*",
   })
 );
 app.use(express.json());
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 
 connectDB();
 
