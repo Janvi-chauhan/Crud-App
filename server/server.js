dedcb1e24c292ff06e7c96cdf7d69f47290abe28
Janvi-chauhan <janvichauhan1223@gmail.com>
2026-05-04 15:35:25 +0530

    Final

--- server/server.js
+++ server/server.js
@@ -8,28 +8,23 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+
+connectDB();
+
+// routes
+app.use("/students", studentRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-const startServer = async () => {
-  try {
-    await connectDB();
-    console.log("Database Connected");
-
-    app.use("/students", studentRoutes);
-
-    const PORT = process.env.PORT || 5000;
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-startServer();
